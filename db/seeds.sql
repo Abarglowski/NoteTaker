5681171f39fb7f49e2a4d158baabb2b52b3914ab
abarglowski <barglowskidrew@gmail.com>
2019-05-05 03:30:59 -0400

    almost done with note taker

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO notes
+  (title, body)
+VALUES
+  ('Today was a nice day', 'Today was such a wonderful day it was so warm!');
+
+INSERT INTO notes
+  (title, body)
+VALUES
+  ('Homework', 'Finish notetaker');
---
+++ public/assets/js/notes.js
@@ -0,0 +1,69 @@
+var $noteList = $("#noteList");
+var $deleteBtn = $(".delete");
+var $submitBtn = $(".submit");
+
+var handleSubmitClick = function(event) {
+  event.preventDefault();
+  var newNote = {
+    title: $("#note-title").val().trim(),
+    body: $("#note-text").val().trim()
+  };
+
+  $.ajax({
+    url: "/api/notes",
+    method: "POST",
+    data: newNote
+  }).then(function(data) {
+    if (data) {
+      alert("Note Saved");
+    }
+    else{
+      alert("Note Did Not Save");
+    }
+    $("#note-title").val("");
+    $("#note-text").val("");
+    }
+  );
+};
+
+var runTableQuery = function() {
+  $.ajax({
+    url: "/api/notes",
+    method: "GET"
+  }).then(function(tableData) {
+    // Loop through and display each of the customers
+    for (var i = 0; i < tableData.length; i++) {
+      var $listItem = $("<li class='list-group-item mt-4'>");
+
+      $listItem.append(
+        $("<h2>").text(tableData[i].title),
+        $("<hr>"),
+        $("<h2>").text(tableData[i].body),
+        $("<button type='submit' class='float-right text-danger delete'>delete note</button>")
+      );
+
+      $noteList.append($listItem);
+    }
+  });
+};
+
+var deleteNotes = function() {
+  console.log("we got here");
+
+  $.ajax({
+    url: "/api/notes",
+    method: "DELETE" 
+  }).then(function(err, res) {
+    if(err) throw err;
+    console.log(res);
+    runTableQuery();
+  });
+};
+
+
+$deleteBtn.on("click", deleteNotes);
+$submitBtn.on("click", handleSubmitClick);
+
+// Run Queries!
+// ==========================================
+runTableQuery();
